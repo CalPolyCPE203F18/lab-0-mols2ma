ba147ce5edfbe80a9fc16d8719caea09aee782f6
Monica Andres <monicaandres@Monicas-MacBook-Pro.local>
2018-09-22 19:27:42 -0700

    am i doing this right

--- lab00.py
+++ lab00.py
@@ -3,25 +3,25 @@ def main():
    x = 5
    y = 'hello'
    z = 9.8
-    
+
    # printing the variables
    print('x:', x, 'y:', y, 'z:', z)
-   
+
    # a list (make an array in java)
    nums = [3, 6, -1, 2]
    for num in nums:
       print(num)
-      
+
    # call a function
-   numFound = char_count(y, 'l')   
+   numFound = char_count(y, 'l')
    print("Found:", numFound)
-   
+
    # a counting for loop
    for i in range(1,11):
       print(i, end=' ')
 
    print()
-   
+
 def char_count(s, c):
    count = 0
    for ch in s:
